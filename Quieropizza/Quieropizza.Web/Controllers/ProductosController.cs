5261e5db9531e4b2b536c3a787c1838d5bcf2214
William Osorto <william.osorto@unah.hn>
2021-02-08 15:18:59 -0600

    Agregamos a una lista de productos

---
+++ Quieropizza/Quieropizza.Web/Controllers/ProductosController.cs
@@ -0,0 +1,35 @@
+﻿using Quieropizza.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Quieropizza.Web.Controllers
+{
+    public class ProductosController : Controller
+    {
+        // GET: Productos
+        public ActionResult Index()
+        {
+            var producto1 = new ProductoModel();
+            producto1.Id = 1;
+            producto1.Descripcion = "Pizza 6 quesos";
+
+            var producto2 = new ProductoModel();
+            producto2.Id = 2;
+            producto2.Descripcion = "Pizza 4 estaciones";
+
+            var producto3 = new ProductoModel();
+            producto3.Id = 3;
+            producto3.Descripcion = "Pizza Jamón y Queso";
+
+            var listadeproductos = new List<ProductoModel>();
+            listadeproductos.Add(producto1);
+            listadeproductos.Add(producto2);
+            listadeproductos.Add(producto3);
+
+            return View(listadeproductos);
+        }
+    }
+}
